c19c965fec5108c926b3237f83bc965bd6cb5ef3
Janier Silva <janier.silva@grupoicts.com.br>
2022-09-28 16:06:59 -0400

    Ler número dentro do intervalo de 100 a 200.

---
+++ src/LerNumero.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class LerNumero {
+    public static void main(String[] args){
+        int numero;
+
+        Scanner leitorScanner = new Scanner(System.in);
+        numero = leitorScanner.nextInt();
+
+        if (numero >= 100 && numero <= 200 ){
+            System.out.println("O número está no intervalo!");
+        } else {
+            System.out.println("O número não está no intervalo!");
+        }
+    }
+}
