04289414d838b87fb706509416db7fcea4c6904f
Janier Silva <janier.silva@grupoicts.com.br>
2022-10-04 08:38:31 -0400

    calculando preço de compra e venda de um produto.

---
+++ src/PrecoCusto.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class PrecoCusto {
+    public static void main(String[] args) {
+        String nomeProduto;
+        float precoCusto;
+        float precoVenda;
+
+        float totalCusto = 0.0f;
+        float totalVenda = 0.0f;
+
+        Scanner leitor = new Scanner(System.in);
+
+        for (int i = 0; i < 40; i++){
+            System.out.println("Digite o nome do Produto:");
+            nomeProduto = leitor.nextLine();
+
+            System.out.println("Digite o preço de custo:");
+            precoCusto = leitor.nextFloat();
+
+            System.out.println("Digite o preço de venda:");
+            precoVenda = leitor.nextFloat();
+
+            totalCusto = totalCusto + precoCusto;
+            totalVenda = totalVenda + precoVenda;
+
+            if (precoCusto == precoVenda){
+                System.out.println("Houve um empate entre o preço de custo e de venda");
+            } else {
+                if (precoCusto > precoVenda){
+                    System.out.println("Prejuízo");
+                } else {
+                    System.out.println("Lucro");
+                }
+            }
+        }
+    }
+}
